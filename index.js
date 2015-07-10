49a429dc3ab8685d4f1e51613a571c05d8769bfd
audibleblink <virtualsnyper@gmail.com>
2015-07-10 13:43:42 -0400

    return promise

--- index.js
+++ index.js
@@ -1,18 +1,12 @@
-var request = require('request')
+var request = require('request-promise')
 var API_URL = 'https://api.new.livestream.com'
 
 module.exports = {
   account: getAccount
 }
 
-function getAccount(number, callback){
+function getAccount(number){
   var endpoint = "accounts"
   var fullUrl  = [API_URL, endpoint, number].join("/")
-  request(fullUrl, function(err, res, body){
-    if(err) {
-      callback(err)
-    } else {
-      callback(null, JSON.parse(body))
-    }
-  })
+  return request(fullUrl)
 }
